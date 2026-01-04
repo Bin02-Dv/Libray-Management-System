1b68b982abe05828e2c320d4bc2aa98f2dfc60f9
Bin02-Dv <alaminbinahmad@gmail.com>
2026-01-04 11:37:44 -0800

    I reconfigure my DB and models

--- LYMS/settings.py
+++ LYMS/settings.py
@@ -122,27 +122,27 @@ LOGGING = {
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("POSTGRES_DATABASE"),
-        "USER": os.environ.get("POSTGRES_USER"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-        "HOST": os.environ.get("POSTGRES_HOST"),
-        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
-# Only require SSL if NOT local
-if os.environ.get("POSTGRES_HOST") and os.environ.get("POSTGRES_HOST") != "localhost":
-    DATABASES["default"]["OPTIONS"] = {"sslmode": "require"}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.environ.get("POSTGRES_DATABASE"),
+#         "USER": os.environ.get("POSTGRES_USER"),
+#         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+#         "HOST": os.environ.get("POSTGRES_HOST"),
+#         "PORT": os.environ.get("POSTGRES_PORT", "5432"),
+#     }
+# }
+
+# # Only require SSL if NOT local
+# if os.environ.get("POSTGRES_HOST") and os.environ.get("POSTGRES_HOST") != "localhost":
+#     DATABASES["default"]["OPTIONS"] = {"sslmode": "require"}
 
 
 # Password validation
--- LYMSApp/models.py
+++ LYMSApp/models.py
@@ -1,9 +1,32 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from cloudinary.models import CloudinaryField
 from django.utils import timezone
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
 
 class AuthModel(AbstractUser):
     username = None
@@ -27,7 +50,7 @@ class Books(models.Model):
     ISBN = models.CharField(max_length=20, unique=True)
     publisher = models.CharField(max_length=100)
     category = models.CharField(max_length=50)
-    cover_image = CloudinaryField('LBRY/books', blank=True, null=True)
+    cover_image = CloudinaryField('LBRY', folder='LBRY/books', blank=True, null=True)
 
     def available_copies(self):
         return self.book_copies.filter(is_available=True).count()
