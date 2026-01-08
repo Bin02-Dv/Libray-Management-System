74462faabe0a175a5dbb3dcf8755191203431502
Bin02-Dv <alaminbinahmad@gmail.com>
2026-01-08 10:20:28 +0100

    1/8/2026 - 10:20am

--- LYMS/settings.py
+++ LYMS/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-qp7fk4pc8a1d@749jjvp%z(%i7%z&^yfx0=5b5y@0lt8_$sam6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
